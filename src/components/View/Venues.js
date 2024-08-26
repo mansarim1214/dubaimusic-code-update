9820733a5126e38b35756cdd5e9712568d40074d
mansarim1214 <mansarimtabasum@gmail.com>
2024-08-26 10:32:19 +0500

    Fixed warnings

--- src/components/View/Venues.js
+++ src/components/View/Venues.js
@@ -39,11 +39,11 @@ const Venues = () => {
   const groupedVenues = groupVenuesByCategory();
   console.log('Grouped Venues:', groupedVenues); // Debugging
 
-  const responsive = {
-    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
-    tablet: { breakpoint: { max: 1024, min: 464 }, items: 3 },
-    mobile: { breakpoint: { max: 464, min: 0 }, items: 2 },
-  };
+  // const responsive = {
+  //   desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
+  //   tablet: { breakpoint: { max: 1024, min: 464 }, items: 3 },
+  //   mobile: { breakpoint: { max: 464, min: 0 }, items: 2 },
+  // };
 
   return (
     <div className="bg-custom">
