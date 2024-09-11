c0bacefd4ced885f63960c9f8ae96e72e271682c
mansarim1214 <mansarimtabasum@gmail.com>
2024-09-12 01:07:52 +0500

    Updated Location in venue model

---
+++ Venue.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const VenueSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
+  category: { type: String },
+  featuredImage: { type: String, required: true },
+  gallery: { type: [String], default: [] }, // Array of strings for gallery images
+});
+
+module.exports = mongoose.model('Venue', VenueSchema);
