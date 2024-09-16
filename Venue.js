01b6824b639af728c5a13bc58691d435e4274310
mansarim1214 <mansarimtabasum@gmail.com>
2024-09-16 18:54:17 +0500

    Updated Venues Routes and Models

--- Venue.js
+++ Venue.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const VenueSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String },
+  description: { type: String },
   location: { type: String },
   category: { type: String },
-  featuredImage: { type: String, required: true },
+  featuredImage: { type: String },
   gallery: { type: [String], default: [] }, // Array of strings for gallery images
 });
 
